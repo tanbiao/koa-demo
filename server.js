00ea1e53f929ab5e12aa96e969927d0d7b74e5d8
Json <fxj@acewill.cn>
2021-03-16 12:33:13 +0800

    husky测试

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const config = require("./config/app");
 const Koa = require("koa");
 const routers = require("./router/index");
-const middleware = require('./middleware/index');
+const middleware = require("./middleware/index");
 
 const app = new Koa(); //new Koa
 
@@ -12,3 +12,12 @@ app.use(routers.routes(), routers.allowedMethods());
 
 app.listen(config.port);
 console.log(`the server is start at port ${config.port}`);
+
+let a = 5;
+let b = 45;
+
+if (b > a) {
+    if (b > a) {
+        console.log("hello");
+    }
+}
